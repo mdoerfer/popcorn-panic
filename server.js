916ff2fc2a2e92570aaab94eac5bcb08b053281a
tapiwan <herkuleshtc@googlemail.com>
2017-05-28 11:41:37 +0200

    Using http to serve

--- server.js
+++ server.js
@@ -1,26 +1,12 @@
 var util = require('util'),
-   io = require('socket.io');
+   server = require('http').createServer(),
+   io = require('socket.io')(server);
 
-var socket,
-   players;
-
-(function init() {
-   players = [];
-
-   socket = io.listen(80);
-   socket.configure(function() {
-      socket.set('transports', [
-         'websocket'
-      ]);
-   });
-
-   setEventHandlers();
-})();
-
-function setEventHandlers() {
-   socket.sockets.on('connection', onSocketConnection);
-}
+io.sockets.on('connection', onSocketConnection);
 
 function onSocketConnection(client) {
-   util.log('New client has connected. ID: ' + client.id);
+   util.log('New client connected. ID: ' + client.id);
 }
+
+util.log('Server started.');
+server.listen(80);
