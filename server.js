75d2f18bca93c8611ab4cb1e282357fc85e54064
tapiwan <herkuleshtc@googlemail.com>
2017-05-28 12:38:06 +0200

    Refactored server

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
+/**
+ * Require dependencies
+ */
 var util = require('util'),
    server = require('http').createServer(),
    io = require('socket.io')(server);
 
-io.sockets.on('connection', onSocketConnection);
+/**
+ * Initialize server
+ * This function runs automatically when the server is started
+ * and runs all the necessary sub-functions
+ */
+(function () {
+   initializeSockets();
+   startServer();
+})();
 
-function onSocketConnection(client) {
+/**
+ * Initialize sockets
+ */
+function initializeSockets() {
+   util.log('Sockets initialized.');
+
+   io.sockets.on('connection', onClientConnection);
+}
+
+/**
+ * Handle client connection
+ *
+ * @param client [The client socket that connected]
+ */
+function onClientConnection(client) {
    util.log('New client connected. ID: ' + client.id);
 }
 
-util.log('Server started.');
-server.listen(80);
+/**
+ * Start the gameserver
+ */
+function startServer() {
+   util.log('Server started.');
+
+   server.listen(80);
+}
