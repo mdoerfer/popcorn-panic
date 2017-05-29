242035c6312fca618d220232f52419e24922763b
tapiwan <herkuleshtc@googlemail.com>
2017-05-29 15:49:25 +0200

    Implemented gitignore for WebStorm

--- server.js
+++ server.js
@@ -13,14 +13,22 @@ var clientEvents = require('./events/client');
 
 /**
  * Initialize server
+ *
  * This function runs automatically when the server is started
  */
 (function() {
-   io.set('origins', '*:*');
-
-   io.sockets.on('connection', clientEvents.onClientConnection);
-
-   util.log('Starting server...');
-   server.listen(80);
-   util.log('Server started.');
+   initializeSockets();
+   startServer();
 })();
+
+function initializeSockets() {
+   io.sockets.on('connection', clientEvents.onClientConnection);
+}
+
+function startServer() {
+   util.log('Starting server...');
+
+   server.listen(80);
+
+   util.log('Server started.');
+}
