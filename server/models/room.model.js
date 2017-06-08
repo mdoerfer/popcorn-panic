be9904eb676798dde40d112573e79e8d4ee376d4
tapiwan <herkuleshtc@googlemail.com>
2017-06-08 21:27:48 +0200

    Moved role model functions to prototype

--- server/models/room.model.js
+++ server/models/room.model.js
@@ -1,85 +1,86 @@
 const _ = require('./../util/util');
 
 const Room = function(roomName) {
-    var name = roomName,
-        players = [],
-        mode = '',
-        map = '',
-        maxPlayers = 4;
+    this.name = roomName;
+    this.players = [];
+    this.mode = '';
+    this.map = '';
+    this.maxPlayers = 4;
+};
 
-    this.setName = function(roomName) {
-        name = roomName;
-    };
+Room.prototype.setName = function(roomName) {
+    this.name = roomName;
+};
 
-    this.addPlayer = function(playerId) {
-        var addedPlayer = false;
+Room.prototype.addPlayer = function(playerId) {
+    var addedPlayer = false;
 
-        if(!this.isFull() && !this.hasPlayer(playerId)) {
-            players.push(playerId);
+    if(!this.isFull() && !this.hasPlayer(playerId)) {
+        this.players.push(playerId);
 
-            addedPlayer = true;
+        addedPlayer = true;
+    }
+
+    return addedPlayer;
+};
+
+Room.prototype.removePlayer = function(playerId) {
+    var self = this;
+    var removedPlayer = false;
+
+    _.foreach(this.players, function(index) {
+        if(this.getId() === playerId) {
+            self.players.splice(index, 1);
+
+            removedPlayer = true;
         }
+    });
 
-        return addedPlayer;
-    };
+    return removedPlayer;
+};
 
-    this.removePlayer = function(playerId) {
-        var removedPlayer = false;
+Room.prototype.setMode = function(roomMode) {
+    this.mode = roomMode;
+};
 
-        _.foreach(players, function(index) {
-            if(this.getId() === playerId) {
-                players.splice(index, 1);
+Room.prototype.setMap = function(roomMap) {
+    this.map = roomMap;
+};
 
-                removedPlayer = true;
-            }
-        });
+Room.prototype.getName = function() {
+    return this.name;
+};
 
-        return removedPlayer;
-    };
+Room.prototype.getPlayers = function() {
+    return this.players;
+};
 
-    this.setMode = function(roomMode) {
-        mode = roomMode;
-    };
+Room.prototype.getMode = function() {
+    return this.mode;
+};
 
-    this.setMap = function(roomMap) {
-        map = roomMap;
-    };
+Room.prototype.getMap = function() {
+    return this.map;
+};
 
-    this.getName = function() {
-        return name;
-    };
+Room.prototype.getMaxPlayers = function() {
+    return this.maxPlayers;
+};
 
-    this.getPlayers = function() {
-        return players;
-    };
+Room.prototype.isFull = function() {
+    return this.players.length >= this.maxPlayers;
+};
 
-    this.getMode = function() {
-        return mode;
-    };
+Room.prototype.isEmpty = function() {
+    return this.players.length === 0;
+};
 
-    this.getMap = function() {
-        return map;
-    };
+Room.prototype.wouldBeEmpty = function() {
+    return (this.players.length - 1) === 0;
+};
 
-    this.getMaxPlayers = function() {
-        return this.maxPlayers;
-    };
-
-    this.isFull = function() {
-        return players.length >= maxPlayers;
-    };
-
-    this.isEmpty = function() {
-        return players.length === 0;
-    };
-
-    this.wouldBeEmpty = function() {
-        return (players.length - 1) === 0;
-    };
-
-    this.hasPlayer = function(playerId) {
-        return players.indexOf(playerId) !== -1;
-    };
+Room.prototype.hasPlayer = function(playerId) {
+    return this.players.indexOf(playerId) !== -1;
 };
 
 module.exports = Room;
