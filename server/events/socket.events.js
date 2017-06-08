511ff0d9b56d6d07d0d8d6b51586f96120cc2fd4
tapiwan <dirk.englert@web.de>
2017-06-08 10:37:41 +0200

    Added event handlers to server

--- server/events/socket.events.js
+++ server/events/socket.events.js
@@ -11,18 +11,102 @@ exports.onSocketConnection = function (socket) {
     util.log('SOCKET_ID: ' + socket.id);
     util.log('SOCKET_TRANSPORT: ' + socket.client.conn.transport.constructor.name);
 
-    //Bind EventHandlers
-    socket.on('disconnect', exports.onSocketDisconnect);
+    bindEventHandlers();
 };
 
 /**
+ * Bind all event handlers
+ */
+function bindEventHandlers() {
+    socket.on('disconnect', exports.onSocketDisconnect);
+    socket.on('signup', exports.onSignup);
+    socket.on('login', exports.onLogin);
+    socket.on('join-lobby', exports.onJoinLobby);
+    socket.on('get-rooms', exports.onGetRooms);
+    socket.on('create-room', exports.onCreateRoom);
+    socket.on('join-room', exports.onJoinRoom);
+    socket.on('leave-room', exports.onLeaveRoom);
+    socket.on('change-map', exports.onChangeMap);
+    socket.on('change-mode', exports.onChangeMode);
+    socket.on('start-game', exports.onStartGame);
+}
+
+/**
  * Handle socket disconnect
- *
- * this = socket
  */
 exports.onSocketDisconnect = function () {
     util.log();
     util.log('SOCKET_DISCONNECTED.');
     util.log('SOCKET_ID: ' + this.id);
     util.log('SOCKET_TRANSPORT: ' + this.client.conn.transport.constructor.name);
+};
+
+/**
+ * On 'signup'
+ */
+exports.onSignup = function (payload) {
+    this.emit('signup-done', {});
+};
+
+/**
+ * On 'login'
+ */
+exports.onLogin = function (payload) {
+    this.emit('login-done', {});
+};
+
+/**
+ * On 'join-lobby'
+ */
+exports.onJoinLobby = function (payload) {
+    this.emit('lobby-joined', {});
+};
+
+/**
+ * On 'get-rooms'
+ */
+exports.onGetRooms = function (payload) {
+    this.emit('rooms-available', {});
+};
+
+/**
+ * On 'create-room'
+ */
+exports.onCreateRoom = function (payload) {
+    this.emit('room-created', {});
+};
+
+/**
+ * On 'join-room'
+ */
+exports.onJoinRoom = function (payload) {
+    this.emit('room-joined', {});
+};
+
+/**
+ * On 'leave-room'
+ */
+exports.onLeaveRoom = function (payload) {
+    this.emit('room-left', {});
+};
+
+/**
+ * On 'change-map'
+ */
+exports.onChangeMap = function (payload) {
+    this.emit('map-changed', {});
+};
+
+/**
+ * On 'change-mode'
+ */
+exports.onChangeMode = function (payload) {
+    this.emit('mode-changed', {});
+};
+
+/**
+ * On 'start-game'
+ */
+exports.onStartGame = function (payload) {
+    this.emit('game-started', {});
 };
