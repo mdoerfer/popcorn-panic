8b5c4aba8043fc0c756229f403f8333960443c83
tapiwan <dirk.englert@web.de>
2017-06-08 12:00:23 +0200

    Refactored server

--- server/server.js
+++ server/server.js
@@ -6,15 +6,16 @@ var util = require('util'),
     io = require('socket.io')(server);
 
 /**
+ * Require models
+ */
+var Player = require('./models/player.model'),
+    Room = require('./models/room.model');
+
+/**
  * Global variables
  */
 var players = [],
     rooms = [];
-
-/**
- * Require events
- */
-var socketEvents = require('./events/socket.events');
 
 /**
  * Initialize server
@@ -30,7 +31,7 @@ var socketEvents = require('./events/socket.events');
  * Establish socket connection
  */
 function initializeSockets() {
-    io.sockets.on('connection', socketEvents.onSocketConnection);
+    io.sockets.on('connection', onSocketConnection);
 }
 
 /**
@@ -42,4 +43,137 @@ function startServer() {
     server.listen(80, function () {
         util.log('SERVER STARTED.');
     });
+}
+
+/**
+ * Handle client connection
+ *
+ * @param socket [The socket connection]
+ */
+function onSocketConnection(socket) {
+    util.log();
+    util.log('NEW_SOCKET_CONNECTED.');
+    util.log('SOCKET_ID: ' + socket.id);
+    util.log('SOCKET_TRANSPORT: ' + socket.client.conn.transport.constructor.name);
+
+    //Add new player
+    players.push(new Player(socket.id));
+    util.log(players);
+
+    bindEventHandlers();
+}
+
+/**
+ * Bind all event handlers
+ */
+function bindEventHandlers() {
+    socket.on('disconnect', onSocketDisconnect);
+    socket.on('choose-name', onChooseName());
+    socket.on('join-lobby', onJoinLobby);
+    socket.on('get-rooms', onGetRooms);
+    socket.on('create-room', onCreateRoom);
+    socket.on('join-room', onJoinRoom);
+    socket.on('leave-room', onLeaveRoom);
+    socket.on('change-map', onChangeMap);
+    socket.on('change-mode', onChangeMode);
+    socket.on('start-game', onStartGame);
+}
+
+/**
+ * Handle socket disconnect
+ */
+function onSocketDisconnect() {
+    util.log();
+    util.log('SOCKET_DISCONNECTED.');
+    util.log('SOCKET_ID: ' + this.id);
+    util.log('SOCKET_TRANSPORT: ' + this.client.conn.transport.constructor.name);
+
+    for (var i = 0; i < players.length; i++) {
+        if (players[i].id === this.id) {
+            players.slice(i, 1);
+        }
+    }
+
+    util.log(players);
+}
+
+/**
+ * On 'choose-name'
+ *
+ * @param payload Contains the data emitted by the client
+ */
+function onChooseName(payload) {
+    this.emit('name-chosen', {});
+}
+
+/**
+ * On 'join-lobby'
+ *
+ * @param payload Contains the data emitted by the client
+ */
+function onJoinLobby(payload) {
+    this.emit('lobby-joined', {});
+}
+
+/**
+ * On 'get-rooms'
+ *
+ * @param payload Contains the data emitted by the client
+ */
+function onGetRooms(payload) {
+    this.emit('rooms-available', {});
+}
+
+/**
+ * On 'create-room'
+ *
+ * @param payload Contains the data emitted by the client
+ */
+function onCreateRoom(payload) {
+    this.emit('room-created', {});
+}
+
+/**
+ * On 'join-room'
+ *
+ * @param payload Contains the data emitted by the client
+ */
+function onJoinRoom(payload) {
+    this.emit('room-joined', {});
+}
+
+/**
+ * On 'leave-room'
+ *
+ * @param payload Contains the data emitted by the client
+ */
+function onLeaveRoom(payload) {
+    this.emit('room-left', {});
+}
+
+/**
+ * On 'change-map'
+ *
+ * @param payload Contains the data emitted by the client
+ */
+function onChangeMap(payload) {
+    this.emit('map-changed', {});
+}
+
+/**
+ * On 'change-mode'
+ *
+ * @param payload Contains the data emitted by the client
+ */
+function onChangeMode(payload) {
+    this.emit('mode-changed', {});
+}
+
+/**
+ * On 'start-game'
+ *
+ * @param payload Contains the data emitted by the client
+ */
+function onStartGame(payload) {
+    this.emit('game-started', {});
 }
