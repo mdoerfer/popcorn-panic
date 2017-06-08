3f55d8610a800ea1841d13f1c3cf496fa047f295
tapiwan <herkuleshtc@googlemail.com>
2017-06-08 21:22:23 +0200

    Changed back to public variables

--- server/models/player.model.js
+++ server/models/player.model.js
@@ -1,102 +1,102 @@
 const _ = require('./../util/util');
 
 const Player = function(playerId) {
-    var id = playerId,
-        name = 'Unknown Unicorn',
-        x = 0,
-        y = 0,
-        z = 0,
-        pressure = 0,
-        character = 'Default',
-        maxPressure = 100;
+    this.id = playerId;
+    this.name = 'Unknown Unicorn';
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+    this.pressure = 0;
+    this.character = 'Default';
+    this.maxPressure = 100;
 
     this.setId = function(playerId) {
-        id = playerId;
+        this.id = playerId;
     };
 
     this.setName = function(playerName) {
-        name = playerName;
+        this.name = playerName;
     };
 
     this.setPressure = function(playerPressure) {
-        pressure = playerPressure;
+        this.pressure = playerPressure;
     };
 
     this.setCharacter = function(playerCharacter) {
-        character = playerCharacter;
+        this.character = playerCharacter;
     };
 
     this.setLocation = function(playerX, playerY, playerZ) {
-        x = playerX;
-        y = playerY;
-        z = playerZ;
+        this.x = playerX;
+        this.y = playerY;
+        this.z = playerZ;
     };
 
     this.setX = function(playerX) {
-        x = playerX;
+        this.x = playerX;
     };
 
     this.setY = function(playerY) {
-        y = playerY;
+        this.y = playerY;
     };
 
     this.setZ = function(playerZ) {
-        z = playerZ;
+        this.z = playerZ;
     };
 
     this.getName = function() {
-        return name;
+        return this.name;
     };
 
     this.getId = function() {
-        return id;
+        return this.id;
     };
 
     this.getPressure = function() {
-        return pressure;
+        return this.pressure;
     };
 
     this.getCharacter = function() {
-        return character;
+        return this.character;
     };
 
     this.getLocation = function() {
         return {
-            x: x,
-            y: y,
-            z: z
+            x: this.x,
+            y: this.y,
+            z: this.z
         };
     };
 
     this.getX = function() {
-        return x;
+        return this.x;
     };
 
     this.getY = function() {
-        return y;
+        return this.y;
     };
 
     this.getZ = function() {
-        return z;
+        return this.z;
     };
 
     this.takeDamage = function(amount) {
-        var newPressure = pressure + amount;
+        var newPressure = this.getPressure() + amount;
 
-        if(newPressure > maxPressure) {
-            pressure = maxPressure;
+        if(newPressure > this.maxPressure) {
+            this.pressure = this.maxPressure;
         }
         else {
-            pressure = newPressure;
+            this.pressure = newPressure;
         }
     };
 
     this.reset = function() {
-        pressure = 0;
-        character = 'Default';
-        x = 0;
-        y = 0;
-        z = 0;
+        this.pressure = 0;
+        this.character = 'Default';
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
     };
 };
 
