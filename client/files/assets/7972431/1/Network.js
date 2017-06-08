015fa27a1fbee0c8def25a0f2ab7c3ee7d26f508
tapiwan <herkuleshtc@googlemail.com>
2017-06-08 22:14:02 +0200

    Updated client

--- client/files/assets/7972431/1/Network.js
+++ client/files/assets/7972431/1/Network.js
@@ -14,6 +14,119 @@ var client = client || function() {
         else {
             //Connect for first time
             self.socket = io.connect(game.config.socket.host);
+            
+            self.socket.on('name-chosen', function(payload) {
+                if(payload.state === 'success') {
+                    self.socket.emit('join-lobby');
+                    console.info('Name changed.');
+                    
+                    console.group('Payload');
+                    console.log(payload.data);
+                    console.groupEnd();
+                    
+                }
+                else {
+                    console.log('Error during name change');
+                } 
+            });
+            
+            self.socket.on('lobby-joined', function(payload) {
+               if(payload.state === 'success') {
+                   //TODO: Change scene to lobby and append data accordingly
+                    console.info('Lobby joined');
+                   
+                   console.group('Payload');
+                    console.log(payload.data);
+                    console.groupEnd();
+                }
+                else {
+                    console.log('Error joining lobby');
+                }
+            });
+            
+            self.socket.on('room-created', function(payload) {
+                if(payload.state === 'success') {
+                    self.socket.emit('join-room', {
+                        name: payload.data.room.name
+                    });
+                    
+                    console.info('Created room');
+                    
+                    console.group('Payload');
+                    console.log(payload.data);
+                    console.groupEnd();
+                }
+                else {
+                    console.log('Error during room creation');
+                }
+            });
+            
+            self.socket.on('room-joined', function(payload) {
+               if(payload.state === 'success') {
+                    //TODO: Change scene to room and append data accordingly
+                    console.info('Joined room');
+                   
+                   console.group('Payload');
+                    console.log(payload.data);
+                    console.groupEnd();
+                }
+                else {
+                    console.log('Error joining room');
+                }
+            });
+            
+            self.socket.on('room-left', function(payload) {
+               if(payload.state === 'success') {
+                    self.socket.emit('join-lobby');
+                   console.info('Left room');
+                   
+                   console.group('Payload');
+                    console.log(payload.data);
+                    console.groupEnd();
+                }
+                else {
+                    console.log('Error leaving room');
+                }
+            });
+            
+            self.socket.on('game-started', function(payload) {
+               if(payload.state === 'success') {
+                    console.info('Game started');
+                   
+                   console.group('Payload');
+                    console.log(payload.data);
+                    console.groupEnd();
+                }
+                else {
+                    console.info('Error starting game');
+                } 
+            });
+            
+            self.socket.on('mode-changed', function(payload) {
+               if(payload.state === 'success') {
+                    console.info('Changed mode');
+                   
+                   console.group('Payload');
+                    console.log(payload.data);
+                    console.groupEnd();
+                }
+                else {
+                    console.log('Error changing mode');
+                }
+            });
+            
+            self.socket.on('map-changed', function(payload) {
+               if(payload.state === 'success') {
+                    console.info('Changed map');
+                   
+                   console.group('Payload');
+                    console.log(payload.data);
+                    console.groupEnd();
+                }
+                else {
+                    console.log('Error changing map');
+                }
+            });
         }
     };
     
