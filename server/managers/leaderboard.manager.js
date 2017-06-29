f328224dd2304cbef2e6e097069a2272739eb965
tapiwan <dirk.englert@web.de>
2017-06-29 16:29:14 +0200

    Dont add players with 0 kills to leaderboard

--- server/managers/leaderboard.manager.js
+++ server/managers/leaderboard.manager.js
@@ -26,18 +26,25 @@ LeaderboardManager.prototype.getLeaderboard = function() {
  * @param player
  */
 LeaderboardManager.prototype.addPlayerToLeaderboard = function(player) {
+    //Skip if player has 0 kills
+    if(player.kills <= 0) return;
+
+    //Reduce information to be saved
     var boardPlayer = {
         name: player.name,
         kills: player.kills,
         timestamp: new Date().valueOf()
     };
 
+    //Add player to leaderboard
     this.leaderboard.push(boardPlayer);
 
+    //Sort leaderboard anew
     this.leaderboard.sort(function(a, b) {
         return b.kills - a.kills;
     });
-
+    
+    //Reduce leaderboard to max players limit
     this.leaderboard = this.leaderboard.slice(0, this.config.maxPlayersInBoard);
 };
 
