07ecca4b08579d940de3cbeca5961e8c8857f90b
tapiwan <dirk.englert@web.de>
2017-06-30 11:41:50 +0200

    Fixed string error

--- server/server.js
+++ server/server.js
@@ -25,6 +25,19 @@ var game = new GameManager();
 function serverHandler(request, response) {
     util.log(request.url);
 
+    // Website you wish to allow to connect
+    response.setHeader('Access-Control-Allow-Origin', '*');
+
+// Request methods you wish to allow
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+// Request headers you wish to allow
+    response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+// Set to true if you need the website to include cookies in the requests sent
+// to the API (e.g. in case you use sessions)
+    response.setHeader('Access-Control-Allow-Credentials', true);
+
     var filePath = __dirname + '/..' + request.url;
 
     if(filePath === __dirname + '/../')
@@ -60,7 +73,7 @@ function serverHandler(request, response) {
             if(error.code === 'ENOENT') {
                 fs.readFile(__dirname + '/../404.html', function(error, content) {
                     response.writeHead(200, {'Content-Type': contentType});
-                    response.end(content, 'utf-8's);
+                    response.end(content, 'utf-8');
                 });
             }
             else {
