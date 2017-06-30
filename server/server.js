c2bc9b77890d8f99561d8946bed3dc213d7ac9c8
tapiwan <dirk.englert@web.de>
2017-06-30 11:50:10 +0200

    Debug server

--- server/server.js
+++ server/server.js
@@ -25,18 +25,19 @@ var game = new GameManager();
  */
 function serverHandler(request, response) {
     util.log(request.url);
+    util.log(__dirname);
 
     // Website you wish to allow to connect
     response.setHeader('Access-Control-Allow-Origin', '*');
 
-// Request methods you wish to allow
+    // Request methods you wish to allow
     response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-// Request headers you wish to allow
+    // Request headers you wish to allow
     response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-// Set to true if you need the website to include cookies in the requests sent
-// to the API (e.g. in case you use sessions)
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
     response.setHeader('Access-Control-Allow-Credentials', true);
 
     fs.readFile(__dirname + "/../client/index.html", function(error, content) {
