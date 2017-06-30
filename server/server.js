46c034cddb03899a504a368e9dc2c9819d670336
tapiwan <dirk.englert@web.de>
2017-06-30 11:38:18 +0200

    Added serving different content types

--- server/server.js
+++ server/server.js
@@ -19,20 +19,59 @@ var game = new GameManager();
 /**
  * Handling serving static files
  *
- * @param req
- * @param res
+ * @param request
+ * @param response
  */
-function serverHandler(req, res) {
-    fs.readFile(__dirname + '/../client/index.html',
-        function(err, data) {
-            if(err) {
-                res.writeHead(500);
-                return res.end('Error loading index.html');
-            }
+function serverHandler(request, response) {
+    var filePath = __dirname + '/../' + request.url;
 
-            res.writeHead(200);
-            res.end(data);
-        });
+    if(filePath === __dirname + '/../')
+        filePath = __dirname + '/../index.html';
+
+    var extname = path.extname(filePath);
+
+    var contentType = 'text/html';
+
+    switch(extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        if(error) {
+            if(error.code === 'ENOENT') {
+                fs.readFile(__dirname + '/../404.html', function(error, content) {
+                    response.writeHead(200, {'Content-Type': contentType});
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, {'Content-Type': contentType});
+            response.end(content, 'utf-8');
+        }
+    });
 }
 
 /**
